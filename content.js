853ffe2b61940b83c32ac42573f5b57f442f8294
erkin97 <matkaziev.97@gmail.com>
2018-09-27 20:52:46 +0900

    give player correct height andd width

--- content.js
+++ content.js
@@ -22,11 +22,13 @@ function get_video_id(v_url){
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if (request.todo == "unblockVideo") {
       var page_href = window.location.href;
-      new_code = '<iframe class="unblocked" width="971" height="546" src="https://www.youtube.com/embed/'+get_video_id(page_href)+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-      var new_player = this.document.createElement("span");
-      new_player.innerHTML = new_code;
       var myNode = this.document.getElementById("player-container-outer");
       myNode = myNode.parentNode;
+      var player_height = myNode.clientHeight;
+      var player_width = myNode.clientWidth;
+      new_code = '<iframe class="unblocked" width="' + player_width + '" height="' + player_height + '" src="https://www.youtube.com/embed/'+get_video_id(page_href)+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+      var new_player = this.document.createElement("span");
+      new_player.innerHTML = new_code;
       while (myNode.firstChild) {
         myNode.removeChild(myNode.firstChild);
       }
