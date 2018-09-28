44ef7942a1e46ca795ba17bc2078c70d76e581f3
erkin97 <matkaziev.97@gmail.com>
2018-09-28 14:58:05 +0900

    autoupdating

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  if (request.todo == "showPageAction")
-  {
-      chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
+  if (request.todo == "showPageAction") {
+      chrome.tabs.query({active:true,currentWindow: true}, function(tabs) {
           chrome.pageAction.show(tabs[0].id);
       });
   }
@@ -14,3 +13,18 @@ chrome.tabs.onUpdated.addListener(
     }
   }
 );
+
+chrome.runtime.onUpdateAvailable.addListener(function(details) {
+  console.log("updating to version " + details.version);
+  chrome.runtime.reload();
+});
+
+chrome.runtime.requestUpdateCheck(function(status) {
+  if (status == "update_available") {
+    console.log("update pending...");
+  } else if (status == "no_update") {
+    console.log("no update found");
+  } else if (status == "throttled") {
+    console.log("Oops, I'm asking too frequently - I need to back off.");
+  }
+});
